65530625d98368c705a5566b3eb0bfb2d2473f67
Dear_Jao <jvvr2013@gmail.com>
2022-07-05 16:41:32 -0300

    requisito 5 completo

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,14 +2,64 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
-  render() {
-    return (
-      <div data-testid="page-search">
-        Procurar
-        <Header />
-      </div>
-    );
+  constructor() {
+    super();
+
+    this.state = {
+      artistName: '',
+      isSearchBtnDisable: true,
+    };
   }
+
+    handleChange = ({ target }) => {
+      this.setState({
+        artistName: target.value,
+      }, () => {
+        this.handleCheckLength();
+      });
+    };
+
+    handleCheckLength = () => {
+      const { artistName } = this.state;
+
+      const minCaractersNumber = 2;
+
+      if (artistName.length >= minCaractersNumber) {
+        this.setState({
+          isSearchBtnDisable: false,
+        }, () => {});
+      } else {
+        this.setState({
+          isSearchBtnDisable: true,
+        }, () => {});
+      }
+    };
+
+    render() {
+      const { artistName, isSearchBtnDisable } = this.state;
+      return (
+        <div data-testid="page-search">
+          <form action="">
+            <input
+              data-testid="search-artist-input"
+              type="text"
+              name="artistName"
+              value={ artistName }
+              onChange={ this.handleChange }
+            />
+
+            <button
+              data-testid="search-artist-button"
+              type="button"
+              disabled={ isSearchBtnDisable }
+            >
+              Procurar
+            </button>
+          </form>
+          <Header />
+        </div>
+      );
+    }
 }
 
 export default Search;
